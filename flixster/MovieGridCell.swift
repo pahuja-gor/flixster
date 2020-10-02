b676acf95cd35b2847ef0f9ff6bc30c56e67b607
Goral Pahuja <pahuja.gor@gmail.com>
2020-10-02 18:26:32 -0400

    Flix - Part 2

---
+++ flixster/MovieGridCell.swift
@@ -0,0 +1,13 @@
+//
+//  MovieGridCell.swift
+//  flixster
+//
+//  Created by Goral Pahuja on 10/2/20.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    @IBOutlet weak var posterView: UIImageView!
+    
+}
